f44d286a31f73dc138d584f4224fbfb52bfa7fba
smrititiwari13 <smrititiwari1313@gmail.com>
2025-08-28 21:33:07 +0530

    Create OccOdd.cpp

---
+++ OccOdd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    map<int,int> freq;
+    for(int x: arr)
+    {
+       freq[x]++;
+    }
+    for(auto it: freq)
+    {
+        if(it.second %2!=0)
+        cout<<it.first<<endl;
+    }
+    
+
+    return 0;
+}
