b491b9b9fa488ad331dc239892bc8f3e897a6ea8
smrititiwari13 <smrititiwari1313@gmail.com>
2026-03-17 10:22:03 +0530

    Implement Tower of Hanoi solution in C++

---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  
+  int n;
+  cin>>n;
+  cout<<((1<<n)-1);
+    
+    return 0;
+    
+}
