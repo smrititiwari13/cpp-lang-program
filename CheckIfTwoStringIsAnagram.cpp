e31755eaef5cfdc1b24f4672d1929f5630b04005
smrititiwari13 <smrititiwari1313@gmail.com>
2026-03-17 12:11:59 +0530

    Refactor anagram check using sorting method

--- CheckIfTwoStringIsAnagram.cpp
+++ CheckIfTwoStringIsAnagram.cpp
@@ -1,27 +1,13 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-bool isanagram(string s1,string s2)
-{
-    if(s1.size()!=s2.size())
-    return false;
-    vector<int> freq(256,0);
-    for(char c: s1) freq[c]++;
-    for(char c: s2) freq[c]--;
-    for(int count: freq)
-    {
-        if(count!=0)
-        return false;
-    }
-    return true;
-}
 int main()
 {
-    string s1,s2;
-    getline(cin,s1);
-    getline(cin,s2);
-    if(isanagram(s1,s2))
-        cout<<"is anagram";
-    else
-        cout<<"not anagram";
+    string x,y;
+    cin>>x>>y;
+    sort(x.begin(),x.end());
+    sort(y.begin(),y.end());
+    if(x==y) cout<<"anagrams";
+    else cout<<"not anagrams";
     return 0;
 }
